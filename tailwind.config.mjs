ca15df3a245e5ecfe7df2e49a316d35906559b9f
Pawani Kumari <pawanipushpakumari7@gmail.com>
2026-06-03 11:17:23 +0530

    adding images and changing the colour palatte

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        navy: '#0a192f',
-        brown: '#8b4513',
-        offwhite: '#f8f9fa',
-      }
+        primary: "#D92243",
+        secondary: "#21825C",
+        background: "#F1F9F9",
+      },
     },
   },
   plugins: [],
-}
+};
